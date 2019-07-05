85a1588032685b07e18e1ee1a0c5bc83ecee1db5
Loek van der Linde <lind0096@hz.nl>
2019-07-05 13:53:22 +0200

    Add timeout validation to measurement creation

--- app/Http/Controllers/Api/MeasurementController.php
+++ app/Http/Controllers/Api/MeasurementController.php
@@ -4,16 +4,48 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Measurement;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class MeasurementController extends Controller
 {
+    /**
+     * Checks request for validity and stores the input data if it is.
+     *
+     * Returns a JSON message with outcome of validation.
+     *
+     * @param Request $request
+     * @return false|Response|string
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request)
     {
         $validated = $this->validate($request, Measurement::rules());
 
+        if ($this->requestCameBeforeTimeout($validated)) {
+            return new Response(['error' => 'Already added measurement less than 10 minutes ago.'],Response::HTTP_PRECONDITION_FAILED);
+        }
+
         $measurement = Measurement::create($validated);
 
         return json_encode(['message' => 'Measurement created with id ' . $measurement->id]);
+
+    }
+
+    /**
+     * @param array $validated
+     * @return bool
+     */
+    private function requestCameBeforeTimeout(array $validated): bool
+    {
+        $lastMeasurement = Measurement::getLastMeasurementByStationName($validated['station'])->created_at;
+        $requestTimeout = Carbon::now()->addMinutes(env('REQUEST_TIMEOUT_IN_MINUTES'));
+
+        if ($lastMeasurement < $requestTimeout) {
+            return true;
+        }
+
+        return false;
     }
 }
--- app/Measurement.php
+++ app/Measurement.php
@@ -15,10 +15,26 @@ class Measurement extends Model
         'station'
     ];
 
+    /**
+     * Return validation rules for the resource
+     *
+     * @return array
+     */
     public static function rules(): array
     {
         return [
             'station' => 'required|string|max:255'
         ];
+    }
+
+    /**
+     * Returns the latest inserted record of a Measurement filtered by station name
+     *
+     * @param string $name
+     * @return Measurement
+     */
+    public static function getLastMeasurementByStationName(string $name): Measurement
+    {
+        return Measurement::where('station', $name)->orderBy('created_at', 'desc')->first();
     }
 }
