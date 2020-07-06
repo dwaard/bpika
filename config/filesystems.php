93f33b2c2a6ebc5c91f4afc1bfebb0761815896a
julienmaster21 <julienmaster21@gmail.com>
2020-07-06 11:02:26 +0200

    Add support for multiple test stations and write output

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,11 @@ return [
             'root' => storage_path('app/test'),
         ],
 
+        'output' => [
+            'driver' => 'local',
+            'root' => storage_path('app/output'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
