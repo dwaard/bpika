3f4947657ae8d632f4d13760720615a52f03e8a6
Daan de Waard <daan.de.waard@hz.nl>
2020-07-10 16:01:54 +0200

    Fix styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,22 @@
 </head>
 <body>
      <!--TODO FIX ID FOR EVERY STATION-->
-    <canvas id="chart" width="1200" height="600"></canvas>
+<div class="brand">PET waardes van de afgelope 7 dagen</div>
+<div class="container">
+    <div class="row">
+            <div class="box col-12">
+            <canvas id="chart"></canvas>
+            </div>
+    </div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="copyright">Copyright &copy; Burger Participatie in Klimaat Adaptatie (BPIKA) 2020</div>
+            </div>
+        </div>
+    </div>
+</footer>
 
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
