b83279481a201da9d8e4646178ea2cba72b29752
Daan de Waard <daan.de.waard@hz.nl>
2020-09-17 17:02:45 +0200

    Fix unexisting login navigation

--- resources/views/common/master.blade.php
+++ resources/views/common/master.blade.php
@@ -12,13 +12,14 @@
 
 <body>
 <header>
-    <nav class="navbar" >
+    <nav class="navbar">
         <div class="container">
             <div class="navbar-brand">
                 <a href="/" class="navbar-item">
                     <i class="fas fa-hat-wizard"></i>&nbsp;BPIKA
                 </a>
-                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navMenu">
+                <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                   data-target="navMenu">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -30,50 +31,53 @@
                     <h1 class="navbar-item has-text-weight-bold has-text-primary">@yield('page_title')</h1>
                 </div>
                 <div class="navbar-end">
-                    @guest
-                        @if (Route::has('register'))
-                            {{-- show a dropdown to choose between login and register --}}
-                            <div class="navbar-item has-dropdown is-hoverable">
-                                <a class="navbar-link">{{ __('Login') }}</a>
+                    @if(Route::has('login'))
+                        @guest
+                            @if (Route::has('register'))
+                                {{-- show a dropdown to choose between login and register --}}
+                                <div class="navbar-item has-dropdown is-hoverable">
+                                    <a class="navbar-link">{{ __('Login') }}</a>
 
-                                <div class="navbar-dropdown">
-                                    <a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                    <a class="navbar-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <div class="navbar-dropdown">
+                                        <a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                        <a class="navbar-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </div>
                                 </div>
-                            </div>
+                            @else
+                                {{-- show only login --}}
+                                <a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            @endif
                         @else
-                            {{-- show only login --}}
-                            <a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        @endif
-                    @else
-                        <div class="navbar-item">
-                            <div class="navbar-item has-dropdown is-hoverable">
-                                <a class="navbar-link">{{ $user->name }}</a>
+                            <div class="navbar-item">
+                                <div class="navbar-item has-dropdown is-hoverable">
+                                    <a class="navbar-link">{{ $user->name }}</a>
 
-                                <div class="navbar-dropdown">
-                                    @if($user->isAdmin)
-                                        <a class="navbar-item" href="/admin">
-                                            <span class="icon"><i class="fas fa-toolbox"></i></span>
-                                            <span>{{__('Admin')}}</span>
+                                    <div class="navbar-dropdown">
+                                        @if($user->isAdmin)
+                                            <a class="navbar-item" href="/admin">
+                                                <span class="icon"><i class="fas fa-toolbox"></i></span>
+                                                <span>{{__('Admin')}}</span>
+                                            </a>
+                                        @endif
+                                        <a class="navbar-item" href="{{route('account.edit')}}">
+                                            <span class="icon"><i class="fas fa-user-cog"></i></span>
+                                            <span>{{__('Profile')}}</span>
                                         </a>
-                                    @endif
-                                    <a class="navbar-item" href="{{route('account.edit')}}">
-                                        <span class="icon"><i class="fas fa-user-cog"></i></span>
-                                        <span>{{__('Profile')}}</span>
-                                    </a>
-                                    <hr class="dropdown-divider">
-                                    <a class="navbar-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
-                                        <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
-                                        <span>{{__('Logout')}}</span>
-                                    </a>
-                                    <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
+                                        <hr class="dropdown-divider">
+                                        <a class="navbar-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
+                                            <span class="icon"><i class="fas fa-sign-out-alt"></i></span>
+                                            <span>{{__('Logout')}}</span>
+                                        </a>
+                                        <form id="frm-logout" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endguest
+                        @endguest
+                    @endif
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
-
 Route::get('/', function () {
     return view('welcome');
 });
