cb41cdd934376daaee548192df751b1cdcc2a715
julienmaster21 <julienmaster21@gmail.com>
2020-09-30 09:56:24 +0200

    Add enabled filter in DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Station;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
+    /**
+     * @return Application|Factory|View
+     */
     public function index()
     {
-        $stations = Station::all();
+        $stations = Station::where('enabled', 1)->get();
         return view('dashboard', [
             'stations' => $stations
         ]);
